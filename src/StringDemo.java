5be88cc87d071f4c8a7fcf39dadbbc8ec6691844
Bob Schmertz <rschmertz@yahoo.com>
2023-09-17 18:29:49 -0400

    move code outside of functions

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class StringDemo {
 
+    // How we normally do things, letting Java handle everything
     public static void printTotalEasy(int x, int y) {
         System.out.println("Total is " + (x + y));
     }
 
+    // What's really happening under the hood
     public static void printTotalBrokenDown(int x, int y) {
         int total = x + y;
         String totalString = Integer.toString(total);
@@ -19,7 +21,17 @@ public class StringDemo {
         System.out.println("enter 2 numbers");
         int x = scanner.nextInt();
         int y = scanner.nextInt();
-        printTotalEasy(x, y);
-        printTotalBrokenDown(x, y);
+        // printTotalEasy(x, y);
+        // printTotalBrokenDown(x, y);
+
+        // How we normally do things, letting Java handle everything
+        System.out.println("Total is " + (x + y));
+
+        // What's really happening under the hood
+        int total = x + y;
+        String totalString = Integer.toString(total);
+        String outputPreamble = "Total is ";
+        String outputText = outputPreamble + totalString;
+        System.out.println(outputText);
     }
 }
